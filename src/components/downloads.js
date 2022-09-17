37ddea0f106df6b7d275d05de7f0b33cb5d08bda
zebra <rs288442@gmail.com>
2022-09-18 00:44:05 +0530

    -vanillaRequest-

--- src/components/downloads.js
+++ src/components/downloads.js
@@ -3,16 +3,26 @@ import { useLocation } from 'react-router-dom';
 import './master.css';
 import {useEffect, useState} from 'react';
 import Files from './files';
+import Axios from 'axios';
 
 const Download=()=>{
   const location = useLocation();
   const { grade } =location.state==null?"all":location.state;
   const [allFiles, setAllFiles] = useState([])
   useEffect(()=>{
-    console.log("useeffect ran")
-    fetch("https://zebra.42web.io/api.php",{mode:'cors'})
-    .then((response)=>response.json())
-    .then(data => setAllFiles(data))
+    // fetch("http://svsdcardgen.42web.io")
+    // .then((response)=>response.json())
+    // .then(data => setAllFiles(data))
+    // Axios.get('https://zebra.42web.io')
+    // .then(response=>response)
+    // .then(data=> setAllFiles(data.data));
+    var xmlhttp = new XMLHttpRequest();
+      xmlhttp.onload = function() {
+        let datarr=JSON.parse(xmlhttp.responseText);
+        setAllFiles(datarr);
+      }
+    xmlhttp.open('POST',`https://zebra.42web.io/api.php`);
+    xmlhttp.send();
   },[])
 
     return(
