f37f6ed41a7c7990f146277b017b070d31d28c76
zebra <rs288442@gmail.com>
2022-09-17 21:39:37 +0530

    cors

--- src/components/downloads.js
+++ src/components/downloads.js
@@ -13,8 +13,8 @@ const Download=()=>{
     // fetch("https://zebra.42web.io/api.php",{mode:cors})
     // .then((response)=>response.json())
     // .then(data => setAllFiles(data))
-    const response = await fetch('https://zebra.42web.io/api.php', {mode:'cors'});
-    const data = await response.json();
+    const response = fetch('https://zebra.42web.io/api.php', {mode:'cors'});
+    const data = response.json();
     setAllFiles(data);
   },[])
 
