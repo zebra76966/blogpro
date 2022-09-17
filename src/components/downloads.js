ab68c1dcec799315d82d846c4b8d580dceb8e01b
zebra <rs288442@gmail.com>
2022-09-17 20:42:51 +0530

    cors-off

--- src/components/downloads.js
+++ src/components/downloads.js
@@ -10,7 +10,7 @@ const Download=()=>{
   const [allFiles, setAllFiles] = useState([])
   useEffect(()=>{
     console.log("useeffect ran")
-    fetch("https://zebra.42web.io/api.php",{ mode: 'no-cors' })
+    await fetch("https://zebra.42web.io/api.php",{ mode: 'no-cors' })
     .then((response)=>response.json())
     .then(data => setAllFiles(data))
   },[])
